e30ae844febf1d8bcb052270ee96aaf4026f14a9
Vasilis Kalligas <billkall@gmail.com>
2016-10-04 01:16:51 +0300

    Allow clients to set if they want the referring version of each gem in the graph.

--- lib/gem_dependencies_visualizer.rb
+++ lib/gem_dependencies_visualizer.rb
@@ -1,12 +1,12 @@
 require "gem_dependencies_visualizer/version"
 
 module GemDependenciesVisualizer
-  def self.produce_gems_graph(string_input, graph_name = nil)
+  def self.produce_gems_graph(string_input, graph_name = nil, options = {})
      unless string_input.nil?
       g = GraphViz::new( :G, :type => :digraph )
       g[:rankdir] ='LR'
 
-      data = populate_gem_data string_input
+      data = populate_gem_data string_input, options
       populate_gem_graph g, data, graph_name
   	 end
   end
@@ -15,7 +15,7 @@ module GemDependenciesVisualizer
   protected
   #########
 
-  def self.populate_gem_data(gemfile_lock_content)
+  def self.populate_gem_data(gemfile_lock_content, options = {})
     string_array = gemfile_lock_content.gsub("\r\n", "\n").split("\n")
     gem_list_index = 0
     gem_list = {}
@@ -25,17 +25,35 @@ module GemDependenciesVisualizer
         gem_list_index = index + 1
 
         continue = true
-        key = string_array[gem_list_index].gsub(/ \(.*\)/, '').gsub(' ', '')
+
+        if options[:keep_gem_version]
+          key = string_array[gem_list_index].strip
+        else
+          key = clear_gem_name_from_version string_array[gem_list_index]
+        end
+        
         values = []
         gem_list_index += 1
 
         while continue
           if /^    [\S]*( \(.*\))?$/.match string_array[gem_list_index]
             gem_list[key] = values
-            key = string_array[gem_list_index].gsub(/ \(.*\)/, '').gsub(' ', '')
+
+              if options[:keep_gem_version]
+                key = string_array[gem_list_index].strip
+              else
+                key = clear_gem_name_from_version string_array[gem_list_index]
+              end
+
             values = []
           elsif /^      [\S]*( \(.*\))?$/.match string_array[gem_list_index]
-            values << string_array[gem_list_index].gsub(/ \(.*\)/, '').gsub(' ', '')
+            if options[:keep_gem_version]
+              value = string_array[gem_list_index].strip
+            else
+              value = clear_gem_name_from_version string_array[gem_list_index]
+            end
+
+            values << value
           else
             gem_list[key] = values
             continue = false
@@ -70,6 +88,11 @@ module GemDependenciesVisualizer
     graph.output(:png => "#{directory_name}/#{graph_name.nil? ? "graph_sample" : graph_name }.png" )
   end  
 
+  def self.clear_gem_name_from_version(name)
+    name.gsub(/ \(.*\)/, '').gsub(' ', '')
+  end
+
   private_class_method :populate_gem_graph
   private_class_method :populate_gem_data
+  private_class_method :clear_gem_name_from_version
 end
